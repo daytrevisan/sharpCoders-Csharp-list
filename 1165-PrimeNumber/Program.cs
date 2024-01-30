76cdc946aae92246c3b4cf379fe77eb89b723462
Dayane <dayanetm@outlook.com>
2024-01-30 19:54:35 -0300

    if-else-for

---
+++ 1165-PrimeNumber/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PrimeNumber;
+public class Program
+{
+    public static void Main()
+    {
+
+        int teste = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < teste; i++)
+        {
+            int X = int.Parse(Console.ReadLine());
+
+            if (IsPrime(X))
+                Console.WriteLine($"{X} eh primo");
+            else
+                Console.WriteLine($"{X} nao eh primo");
+        }
+    }
+
+    static bool IsPrime(int num)
+    {
+        if (num <= 1) { return false; }
+
+        for (int i = 2; i <= Math.Sqrt(num); i++)
+        {
+            if (num % i == 0) { return false; }
+        }
+        return true;
+
+    }
+}
