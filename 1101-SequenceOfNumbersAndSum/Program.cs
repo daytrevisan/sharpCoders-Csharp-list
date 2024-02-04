bbd6e1b314520e047eb3757c6113cf02cca70099
Dayane <dayanetm@outlook.com>
2024-02-04 11:09:11 -0300

    sequencia-e-soma

---
+++ 1101-SequenceOfNumbersAndSum/Program.cs
@@ -0,0 +1,34 @@
+﻿// Leia um par de números (M e N), retorne em ordem crescente esses números (incluindo M e N) e sua respectiva soma
+// O programa deve encerrar quando um deles for igual ou menor que zero 
+
+using System;
+class URI {
+    static void Main(string[] args) { 
+        while (true) {
+            string[] input = Console.ReadLine().Split();
+            int M = int.Parse(input[0]);
+            int N = int.Parse(input[1]);
+
+            if (M <= 0 || N <= 0)
+                break;
+
+            int min = Math.Min(M, N);
+            int max = Math.Max(M, N);
+
+            PrintSequenceAndSum(min, max);
+        }
+    }
+
+    static void PrintSequenceAndSum(int min, int max) {
+        int sum = 0;
+
+        for (int i = min; i <= max; i++) {
+            Console.Write(i + " ");
+            sum += i;
+        }
+
+        // Imprime a soma no final
+        Console.WriteLine("Sum=" + sum);
+    }
+    
+}
---
+++ 1101-SequenceOfNumbersAndSum/SequenceOfNumbersAndSum-ReadMe.md
@@ -0,0 +1,9 @@
+Read an undetermined number of pairs values M and N (stop when any of these values is less or equal to zero). For each pair, print the sequence from the smallest to the biggest (including both) and the sum of consecutive integers between them (including both).
+
+Input
+The input file contains pairs of integer values M and N. The last line of the file contains a number zero or negative, or both.
+
+Output
+For each pair of numbers, print the sequence from the smallest to the biggest and the sum of these values, as shown below.
+
+![Alt text](image.png)
