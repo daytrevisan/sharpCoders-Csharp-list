5a54262517a1cf4980f49e947fb453ef4e948f3d
Dayane <dayanetm@outlook.com>
2024-02-16 14:30:08 -0300

    fatorial de um número

---
+++ 1153-SimpleFactorial/Program.cs
@@ -0,0 +1,21 @@
+﻿// Leia um número N (0<N<13) e retorne o cálculo de seu fatorial
+
+using System;
+
+namespace SimpleFactorial;
+
+public class Program {
+    public static void Main(){
+
+        Console.Write("Digite um número: ");
+        int input = int.Parse(Console.ReadLine());
+        int fatorial = 1;
+
+        for(int i=input; i>0; i--) {
+            fatorial *= i;
+        }
+
+        Console.WriteLine(fatorial);
+
+    }
+}
