addaf1051cb03a8a141e8f6238352785f40e10f4
Dayane <dayanetm@outlook.com>
2024-04-11 17:01:01 -0300

    conta e soma positivos

---
+++ 1064-PositivesAndAverage/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PositivesAndAverage;
+
+public class Program {
+    public static void Main() {
+
+        int count = 0;
+        double sum = 0;
+
+        for(int _ = 0 ; _ < 6; _++) {
+            double value = double.Parse(Console.ReadLine());
+
+            if(value > 0) {
+                sum += value;
+                count++;
+            }
+        }
+
+        double average = sum / count;
+
+        Console.WriteLine($"{count} valores positivos");
+        Console.WriteLine($"{average:F1}");
+    }
+    
+}
