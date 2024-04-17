9add9f28965195e5d755b7bd4d3987977ed38a5e
Dayane <dayanetm@outlook.com>
2024-04-17 18:50:41 -0300

    multiplos de 13

---
+++ 1132-MultiplesOf13/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MultiplesOf13;
+
+public class Program {
+    
+    static bool IsMultipleOf13(int number) {
+        return number % 13 == 0;
+    }
+    public static void Main() {
+
+        int begin = int.Parse(Console.ReadLine());
+        int end = int.Parse(Console.ReadLine());
+        int sum = 0;
+
+        if(begin > end) {
+            int aux = begin;
+            begin = end;
+            end = aux;
+        }
+
+        for(int i = begin; i <= end; i++) {
+            if(!IsMultipleOf13(i)) {
+                sum += i;
+            }
+        }
+
+        Console.WriteLine(sum);
+    }
+}
