c9e8131ae9aac5a6c73dba8450b93a2b2a422494
Dayane <dayanetm@outlook.com>
2024-04-18 21:04:02 -0300

    numeros com resto 2

---
+++ 1075-RemainingTwo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RemainingTwo;
+
+public class Program {
+    public static void Main(string[] args) {
+
+        int n = int.Parse(Console.ReadLine());
+
+        for(int i = 0; i < 10000; i++) {
+            if(i % n == 2 ) {
+                Console.WriteLine(i);
+            }
+        }
+
+    }
+}
---
+++ 1075-RemainingTwo/RemainingTwo-ReadMe.md
@@ -0,0 +1,9 @@
+Read an integer N. Print all numbers between 1 and 10000, which divided by N will give the rest = 2.
+
+Input
+The input is an integer N (N < 10000)
+
+Output
+Print all numbers between 1 and 10000, which divided by n will give the rest = 2, one per line.
+
+![alt text](image.png)
